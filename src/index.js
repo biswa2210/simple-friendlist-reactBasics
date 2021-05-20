bad4f6cf6a91d43690f55d9328c8b19e84a57ff4
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-20 18:27:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 
 ReactDOM.render(
   <React.StrictMode>
